feec15c9c9f7c82d679d9f03c5edb1b50bc2b70f
soxface <sonyacorcoran@gmail.com>
2019-01-02 22:41:34 +1100

    replacing temporary play variable with a query

--- ch1/index.js
+++ ch1/index.js
@@ -5,8 +5,12 @@ function statement (invoice, plays) {
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   function amountFor(aPerformance, play) {
     let result = 0;
