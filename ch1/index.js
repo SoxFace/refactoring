91616d92c2b0ab9a97f9aa52c2a68904c5f47722
soxface <sonyacorcoran@gmail.com>
2019-01-02 22:50:29 +1100

    remove the play parameter. Step 1 - use the new playFor function inline in amountFor

--- ch1/index.js
+++ ch1/index.js
@@ -4,17 +4,16 @@ function statement (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
-  for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
-
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
 
+  for (let perf of invoice.performances) {
+    let thisAmount = amountFor(perf, playFor(perf));
+
   function amountFor(aPerformance, play) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -29,7 +28,7 @@ function statement (invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
 
     return result;
@@ -38,10 +37,10 @@ function statement (invoice, plays) {
      // add volume credits
      volumeCredits += Math.max(perf.audience - 30, 0);
      // add extra credit for every ten comedy attendees
-     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
      // print line for this order
-     result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+     result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
      totalAmount += thisAmount;
  
   }
