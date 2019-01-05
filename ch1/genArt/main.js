c4a7a7fba4f71dbfff7afd862ef04dde2950796d
soxface <sonyacorcoran@gmail.com>
2019-01-05 21:55:47 +1100

    extracted first switch statement and renamed char, x and y to readable names

--- ch1/genArt/main.js
+++ ch1/genArt/main.js
@@ -10,47 +10,56 @@
 */
 
 var canvas = document.getElementById('keith');
-canvas.width = document.documentElement.clientWidth;
-canvas.height = document.documentElement.clientHeight;
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+
 var context = canvas.getContext('2d');
-context.lineWidth = 10;
-var x = document.documentElement.clientWidth / 2;
-var y = document.documentElement.clientHeight / 2;
+    context.lineWidth = 10;
+
+var widthHalved = document.documentElement.clientWidth / 2;
+var heightHalved = document.documentElement.clientHeight / 2;
 // var rule = "f+f−f−f+f+f+f−fff+fff+fff−f+f−f+f−f−f+f+f+f−f−f+f"
 var rule = "-YF+XFX+FY-";
 var distance = 50;
 var direction = 100;
-var paths = [{ x: x, y: y }];
-for (var j = 0; j < 50; j++) {
-  for (var i = 0; i < rule.length; i++) {
-    var char = rule[i];
-    switch (char) {
-      case 'f': {
-        if (direction === 0) x += distance;
-        else if (direction === 1) y += distance;
-        else if (direction === 2) x -= distance;
-        else if (direction === 3) y -= distance;
-        if (x < 0 || x > document.documentElement.clientWidth)
-          x = getRandomInt(document.documentElement.clientWidth)
-        if (y < 0 || y > document.documentElement.clientHeight)
-          y = getRandomInt(document.documentElement.clientHeight)
-        paths.push({ x: x, y: y })
-        break;
+var paths = [{ x: widthHalved, y: heightHalved }];
+
+function changeDirection() {
+  var character = rule[i];
+  switch (character) {
+    case 'f': {
+      if (direction === 0) widthHalved += distance;
+      else if (direction === 1) heightHalved += distance;
+      else if (direction === 2) widthHalved -= distance;
+      else if (direction === 3) heightHalved -= distance;
+      if (widthHalved < 0 || widthHalved > document.documentElement.clientWidth)
+        x = getRandomInt(document.documentElement.clientWidth)
+      if (heightHalved < 0 || heightHalved > document.documentElement.clientHeight)
+        y = getRandomInt(document.documentElement.clientHeight)
+      paths.push({ x: widthHalved, y: heightHalved })
+      break;
+    }
+    case '+': {
+      direction = (direction + 1) % 4;
+      break;
+    }
+    case '-': {
+      if (direction === 0) {
+        direction = 3;
+      } else {
+        direction = direction - 1;
       }
-      case '+': {
-        direction = (direction + 1) % 4;
-        break;
-      }
-      case '-': {
-        if (direction === 0) {
-          direction = 3;
-        } else {
-          direction = direction - 1;
-        }
-        break;
-      }
+      break;
     }
   }
+}
+
+for (var j = 0; j < 50; j++) {
+  
+  for (var i = 0; i < rule.length; i++) {
+    changeDirection();
+  }
+
   var charIndex = getRandomInt(rule.length - 1);
   var newChar = '';
   switch (rule[charIndex]) {
@@ -67,15 +76,19 @@ for (var j = 0; j < 50; j++) {
       break;
     }
   }
+
   rule = rule.substr(0, charIndex - 1) + newChar + rule.substr(charIndex);
 }
+
 var index = 0;
 context.beginPath();
 context.moveTo(paths[index].x, paths[index].y);
 index++;
+
 var speed = 50;
 var colours = ['white'];
 setTimeout(draw, speed);
+
 function draw() {
   context.beginPath();
   context.moveTo(paths[index - 1].x, paths[index - 1].y);
@@ -87,6 +100,7 @@ function draw() {
     setTimeout(draw, speed);
   }
 }
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
