c882527e6539247c67b1e3b0e65a58648d7f12fb
soxface <sonyacorcoran@gmail.com>
2019-01-13 14:43:10 +1100

    slide statements together, extracted random colour and added inline varible for stroke color

--- ch1/genArt/main.js
+++ ch1/genArt/main.js
@@ -9,13 +9,6 @@
   [ iterations, angle, constants, axiom, rule1, rule2 ]
 */
 
-function setupCanvas() {
-  var canvas = document.getElementById('lSystemArt');
-  canvas.width = document.documentElement.clientWidth;
-  canvas.height = document.documentElement.clientHeight;
-  return canvas;
-}
-
 var context = setupCanvas().getContext("2d");
     context.lineWidth = 10;
 
@@ -26,6 +19,33 @@ var rule = "-YF+XFX+FY-";
 var distance = 50;
 var direction = 100;
 var paths = [{ x: widthHalved, y: heightHalved }];
+console.log(paths);
+
+var index = 0;
+context.beginPath();
+context.moveTo(paths[index].x, paths[index].y);
+index++;
+
+var speed = 50;
+
+function randomColour() {
+  var colours = ["white", "red", "green", "blue"];
+  var result  = colours[getRandomInt(colours.length)];
+  return result;
+}
+
+setTimeout(draw, speed);
+
+function setupCanvas() {
+  var canvas = document.getElementById('lSystemArt');
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+  return canvas;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function changeDirection() {
   var character = rule[i];
@@ -83,27 +103,14 @@ for (var j = 0; j < 50; j++) {
   rule = rule.substr(0, charIndex - 1) + newChar + rule.substr(charIndex);
 }
 
-var index = 0;
-context.beginPath();
-context.moveTo(paths[index].x, paths[index].y);
-index++;
-
-var speed = 50;
-var colours = ['white'];
-setTimeout(draw, speed);
-
 function draw() {
   context.beginPath();
   context.moveTo(paths[index - 1].x, paths[index - 1].y);
-  context.strokeStyle = colours[getRandomInt(colours.length - 1)];
+  context.strokeStyle = randomColour();
   context.lineTo(paths[index].x, paths[index].y);
   context.stroke();
   index++;
   if (index < paths.length - 1) {
     setTimeout(draw, speed);
   }
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
 }
