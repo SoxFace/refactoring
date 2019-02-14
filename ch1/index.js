78c76072dc5961db9cd161d30533cb2d0c5acb30
soxface <sonyacorcoran@gmail.com>
2019-02-14 21:06:09 +1100

    replace loop with pipeline i.e .reduce

--- ch1/index.js
+++ ch1/index.js
@@ -7,19 +7,13 @@ function statement(invoice, plays) {
   return renderPlainText(statementData, plays);
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.amount, 0);
   }
   
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function enrichPerformance(aPerformance) {
